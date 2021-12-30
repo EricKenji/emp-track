7605d17df558912feb39096d1c83b70801ae60a0
Eric Furukawa <eric.furukawa@gmail.com>
2021-12-30 14:07:20 -0800

    made changes to schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,17 @@
 INSERT INTO department (name)
 VALUES
-    ('Computers'),
-    ('Home Entertainment'),
+    ('Management'),
+    ('Sales'),
     ('Customer Service');
 
 INSERT INTO role (title, salary, department_id)
 VALUES
     ('Manager', 100000, 1), 
-    ('Supervisor', 80000, 2), 
+    ('Sales Rep', 80000, 2), 
     ('Associate', 60000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-    ('Charles', 'Anderson', 1, 1),
+    ('Charles', 'Anderson', 1, null),
     ('Rodrigo', 'Gallo', 2, 1),
-    ('Kevin', 'Norman', 3, null);
+    ('Kevin', 'Norman', 3, 1);
